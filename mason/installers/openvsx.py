ad8e7f1f2b2d054c9922cd3f8bc9a37867563d6b
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-05 07:11:22 +0200

    feat: improve openvsx

--- mason/installers/openvsx.py
+++ mason/installers/openvsx.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 from mason.package import Package
-from mason.utility import download_file, extract_file, is_extractable
+from mason.utility import download_file, extract_file
 
 
 def install(pkg: Package) -> None:
-    out_path = Path(*pkg.files)
-    download_file(
-        f"https://open-vsx.org/api/{pkg.purl.namespace}/{pkg.purl.name}/{pkg.purl.version}/file/{pkg.files[0]}",
-        out_path,
-    )
-    if is_extractable(out_path):
+    for file in pkg.files or []:
+        out_path = Path(file)
+        download_file(
+            f"https://open-vsx.org/api/{pkg.purl.namespace}/{pkg.purl.name}/{pkg.purl.version}/file/{file}",
+            out_path,
+        )
         extract_file(out_path)
