2f9324f56c5c1700c769b2aa743f63d4b96313c9
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-05 17:07:46 +0200

    feat: simplify downloading github release

--- mason/utility.py
+++ mason/utility.py
@@ -45,14 +45,14 @@ def download_file(url: str, out_path: Path) -> None:
 
 
 def download_github_release(repo: str, asset: str, version="latest", out_path=Path(".")) -> None:
-    if version != "latest":
-        version = f"tags/{version}"
-    response = requests.get(f"https://api.github.com/repos/{repo}/releases/{version}")
-    response.raise_for_status()
-    download_link = next((a["browser_download_url"] for a in response.json()["assets"] if a["name"] == asset), None)
-    if not download_link:
-        raise ValueError(f"Asset '{asset}' not found in release '{version}'")
-    download_file(download_link, out_path / asset)
+    download_file(
+        (
+            f"https://github.com/{repo}/releases/latest/download{asset}"
+            if version == "latest"
+            else f"https://github.com/{repo}/releases/download/{version}/{asset}"
+        ),
+        out_path / asset,
+    )
 
 
 def select_by_os(unix: Any, windows: Any) -> Any:
