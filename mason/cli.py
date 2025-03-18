094dd0d7f7c8f3d04274f864105332b5ff972922
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-18 15:16:30 +0200

    refactor: inline commnads in cli.py

--- mason/cli.py
+++ mason/cli.py
@@ -1,12 +1,71 @@
 import argparse
 import sys
 from pathlib import Path
+from typing import Any, cast
 
 import argcomplete
 from argcomplete.completers import ChoicesCompleter
 
-from mason import commands, config
+from mason import config
 from mason.context import Context
+from mason.package import Package
+
+
+def install(args: Any, ctx: Context) -> None:
+    if args.update_registries:
+        print("Updating registries...")
+        ctx.update_registries()
+
+    for name in args.package:
+        pkg = ctx.package(name)
+        if not pkg:
+            raise ValueError(f"Package '{name}' not found")
+        if pkg.deprecation:
+            raise ValueError(f"Package '{pkg.name}' is deprecated: {pkg.deprecation}")
+        print(f"Installing '{name}'...")
+        pkg.install()
+
+
+def uninstall(args: Any, ctx: Context) -> None:
+    for name in args.package:
+        pkg = ctx.package(name)
+        if not pkg:
+            raise ValueError(f"Package '{name}' not found")
+        if name not in ctx.installed_package_names:
+            raise ValueError(f"Package '{name}' is not installed")
+        print(f"Uninstalling '{name}'...")
+        pkg.uninstall()
+
+
+def list(ctx: Context) -> None:
+    for pkg in (cast(Package, ctx.package(p)) for p in ctx.installed_package_names):
+        print(f"{pkg.name}@{pkg.purl.version}")
+
+
+def search(args: Any, ctx: Context) -> None:
+    def match(package: Package):
+        return (
+            (not args.category or any(args.category.casefold() == c.casefold() for c in package.categories))
+            and (
+                not args.language
+                or (package.languages and any(args.language.casefold() == l.casefold() for l in package.languages))
+            )
+            and (args.query in package.name or args.query in package.description)
+        )
+
+    for pkg in ctx.packages:
+        if not match(pkg):
+            continue
+        print(f"{pkg.name} {pkg.purl.version}")
+        if pkg.deprecation:
+            print(f"    Deprecation: {pkg.deprecation}")
+        print(f"    Description: {pkg.description}")
+        print(f"    Homepage: {pkg.homepage}")
+        print(f"    Categories: {', '.join(pkg.categories)}")
+        if pkg.languages:
+            print(f"    Languages: {', '.join(pkg.languages)}")
+        print(f"    Licenses: {', '.join(pkg.licenses)}")
+        print()
 
 
 def main():
@@ -32,15 +91,19 @@ def main():
         subparsers = parser.add_subparsers()
 
         parser_install = subparsers.add_parser("install", help="install packages", formatter_class=formatter)
-        parser_install.set_defaults(func=lambda args: commands.install(args, ctx))
+        parser_install.set_defaults(func=lambda args: install(args, ctx))
         parser_install.add_argument("-u", "--update-registries", action="store_true", help="update registries")
         parser_install.add_argument("package", nargs="+").completer = ChoicesCompleter(ctx.package_names)
 
+        parser_uninstall = subparsers.add_parser("uninstall", help="uninstall packages", formatter_class=formatter)
+        parser_uninstall.set_defaults(func=lambda args: uninstall(args, ctx))
+        parser_uninstall.add_argument("package", nargs="+").completer = ChoicesCompleter(ctx.installed_package_names)
+
         parser_list = subparsers.add_parser("list", help="list installed packages", formatter_class=formatter)
-        parser_list.set_defaults(func=lambda _: commands.list(ctx))
+        parser_list.set_defaults(func=lambda _: list(ctx))
 
         parser_search = subparsers.add_parser("search", help="search packages", formatter_class=formatter)
-        parser_search.set_defaults(func=lambda args: commands.search(args, ctx))
+        parser_search.set_defaults(func=lambda args: search(args, ctx))
         parser_search.add_argument("query", nargs="?", default="")
         parser_search.add_argument(
             "-c",
@@ -50,10 +113,6 @@ def main():
             help="specify category for search",
         )
         parser_search.add_argument("-l", "--language", metavar="language", help="specify language for search")
-
-        parser_uninstall = subparsers.add_parser("uninstall", help="uninstall packages", formatter_class=formatter)
-        parser_uninstall.set_defaults(func=lambda args: commands.uninstall(args, ctx))
-        parser_uninstall.add_argument("package", nargs="+").completer = ChoicesCompleter(ctx.installed_package_names)
 
         argcomplete.autocomplete(parser)
 
--- mason/commands/list.py
+++
@@ -1,8 +0,0 @@
-from typing import cast
-from mason.context import Context
-from mason.package import Package
-
-
-def list(ctx: Context) -> None:
-    for pkg in (cast(Package, ctx.package(p)) for p in ctx.installed_package_names):
-        print(f"{pkg.name}@{pkg.purl.version}")
