dad6afd2fbf76ba338fc89b5d7dbee9963365356
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-24 23:53:23 +0200

    feat: list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"sort"
 
+	"github.com/amedoeyes/mason/pkg/context"
+	"github.com/amedoeyes/mason/pkg/receipt"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +13,31 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List installed packages",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("List command called")
+		ctx := cmd.Context().Value(contextKey).(*context.Context)
+
+		receiptsSlice := make([]*receipt.Receipt, 0, len(ctx.Receipts))
+		for _, rct := range ctx.Receipts {
+			receiptsSlice = append(receiptsSlice, rct)
+		}
+
+		sort.Slice(receiptsSlice, func(i, j int) bool {
+			return receiptsSlice[i].Name < receiptsSlice[j].Name
+		})
+
+		maxTypeLen, maxNameLen, maxVerLen := 0, 0, 0
+		for _, rct := range receiptsSlice {
+			maxTypeLen = max(maxTypeLen, len(rct.PrimarySource.PURL.Type))
+			maxNameLen = max(maxNameLen, len(rct.PrimarySource.PURL.Name))
+			maxVerLen = max(maxVerLen, len(rct.PrimarySource.PURL.Version))
+		}
+
+		format := fmt.Sprintf("%%-%ds  %%-%ds  %%-%ds\n", maxTypeLen, maxNameLen, maxVerLen)
+		for _, rct := range receiptsSlice {
+			fmt.Printf(format,
+				rct.PrimarySource.PURL.Type,
+				rct.Name,
+				rct.PrimarySource.PURL.Version,
+			)
+		}
 	},
 }
